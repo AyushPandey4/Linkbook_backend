f8508196ee35a9ef445e43153453337a3bc10701
AyushPandey4 <ayushpandey@gmail.com>
2025-02-19 01:42:46 +0530

    removed test connection

--- connection/db.js
+++ connection/db.js
@@ -5,14 +5,6 @@ const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
-// Test connection
-(async () => {
-  const { data, error } = await supabase.from("users").select("*").limit(1);
-  if (error) {
-    console.error("Supabase connection error:", error.message);
-  } else {
-    console.log("Supabase connected successfully!");
-  }
-})();
+
 
 module.exports = supabase;
